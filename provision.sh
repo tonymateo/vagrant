9796e7f893654ae80e368e2abcdbb0b3e7d71988
= <=>
2016-12-26 16:50:15 +0100

    añadiendo ficheros conf

--- provision.sh
+++ provision.sh
@@ -4,4 +4,13 @@ rm -r /var/www
 rm -r /vagrant/www
 mkdir /vagrant/www
 ln -s /vagrant/www /var/www
-git clone https://github.com/tonymateo/ej_1e_despliegue.git /var/www
+git clone https://github.com/tonymateo/vagrant.git /home/vagrant/config
+cp /home/vagrant/config/sitio1.conf /etc/apache2/sites-available/sitio1.conf
+cp /home/vagrant/config/sitio2.conf /etc/apache2/sites-available/sitio2.conf
+a2ensite sitio1.conf
+a2ensite sitio2.conf
+a2enmod vhost_alias
+service apache2 restart
+cd /
+git clone https://github.com/tonymateo/ej_1e_despliegue.git /var/www/sitio1
+git clone https://github.com/tonymateo/ej_github.git /var/www/sitio2
